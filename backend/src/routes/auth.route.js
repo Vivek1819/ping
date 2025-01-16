7870869c71d1383505b216a54bec4e1a8dbb2709
Vivek1819 <vivekhipparkar@gmail.com>
2025-01-16 10:32:10 +0530

    start

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const router = express.Router();
+
+router.post("/signup",(req, res)=>{
+    res.send("singup");
+});
+
+router.post("/login",(req, res)=>{
+    res.send("login");
+});
+
+router.post("/logout",(req, res)=>{
+    res.send("logout");
+});
+
+export default router;
